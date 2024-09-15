259930a4a38602688843ef8624d42179d5038d99
Daniel Hollberg <alan11374@gmail.com>
2024-09-15 08:00:07 -0400

    Final

--- backend/IncidentManager.py
+++ backend/IncidentManager.py
@@ -60,3 +60,38 @@ for i in range(9,11):
 
 for description in descriptions:
     insert_data_from_txt(description)
+
+returned_value = recognize_from_microphone()
+
+speech_to_text_dict = convert_response_to_dict(returned_value)
+
+prediction_dict = {
+    'age': [speech_to_text_dict['age']],
+    'num_people': [speech_to_text_dict['num_people']],
+    'mentioned_medical': [speech_to_text_dict['mentioned_medical']],
+    'mentioned_violence': [speech_to_text_dict['mentioned_violence']],
+    'mentioned_fire': [speech_to_text_dict['mentioned_fire']],
+    'mentioned_vehicular': [speech_to_text_dict['mentioned_vehicular']],
+    'mentioned_mental_health': [speech_to_text_dict['mentioned_mental_health']],
+    'mentioned_natural_disasters': [speech_to_text_dict['mentioned_natural_disasters']],
+    'mentioned_environmental_hazards': [speech_to_text_dict['mentioned_environmental_hazards']],
+    'mentioned_suspicious_activity': [speech_to_text_dict['mentioned_suspicious_activity']],
+    'mentioned_urgency': [speech_to_text_dict['mentioned_urgency']]
+}
+
+
+# print("dict to be used to get severity score")
+# print(prediction_dict)
+severity_score = float(xgb_predict.predict_severity(prediction_dict))
+# print("severity of incident:", severity_score)
+# print()
+
+speech_to_text_dict["severity"] = severity_score
+
+# print("document dict to be inserted into db")
+# print(speech_to_text_dict)
+# print()
+
+
+dbm = DatabaseManager()
+dbm.insert_incident(speech_to_text_dict)
